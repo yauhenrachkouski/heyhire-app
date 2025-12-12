0368335d011095bb1a2992b008a4c1248a6063f6
Yauhen Rachkouski <yauhenrachkouski@gmail.com>
2025-12-11 22:01:04 -0500

    fix migration

--- migrations/0012_outstanding_machine_man.sql
+++ migrations/0012_outstanding_machine_man.sql
@@ -1,3 +1,2 @@
-ALTER TABLE "candidates" ADD COLUMN "embedding" vector(1536);--> statement-breakpoint
-CREATE INDEX "embedding_idx" ON "candidates" USING hnsw ("embedding" vector_cosine_ops);--> statement-breakpoint
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE INDEX "search_name_idx" ON "search" USING gin ("name" gin_trgm_ops);
