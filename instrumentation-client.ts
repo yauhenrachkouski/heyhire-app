0bbf9c57693996f78423a83c7de0949cb4e2cebe
Yauhen Rachkouski <yauhenrachkouski@gmail.com>
2026-01-09 09:45:08 -0500

    update tracking posthog

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,8 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: "https://us.posthog.com",
   defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+  capture_exceptions: true,
   debug: process.env.NEXT_PUBLIC_POSTHOG_DEBUG === "true",
+  cross_subdomain_cookie: true,
+  persistence: 'localStorage+cookie',
 });
